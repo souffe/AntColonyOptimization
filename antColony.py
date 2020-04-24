b2b59395a5bf6fbe00bc8ad0b3b9083cf9f4078e
mat.redzia@gmail.com <mat.redzia@gmail.com>
2020-04-24 21:10:02 +0200

    Ant colony tests

--- antColony.py
+++ antColony.py
@@ -36,6 +36,9 @@ class Road(object):
     def getDistance(self):
         return self.distance
 
+    def getPheromone(self):
+        return self.pheromone
+
 class Ant(object):
     def __init__(self, start_city, pheromone):
         self.Qpheromone = pheromone
@@ -43,6 +46,31 @@ class Ant(object):
         self.cities = list()
         self.totalDistance = 0
         self.start_city_id = start_city
+        self.currentCity = start_city
+
+    def resetTravelData(self):
+        self.roads = list()
+        self.cities = list()
+        self.totalDistance = 0
+    
+    # Move along the road
+    def move(self, road):
+        # Remember this city as already visited
+        self.cities.append(self.currentCity)
+        
+        # Add distance of this road to total distance
+        addDistance = road.getDistance()
+        self.totalDistance = self.totalDistance + addDistance
+        
+        newCity = road.city1_id
+        if newCity == self.currentCity:
+            newCity = road.city2_id
+        
+        # Change the current city
+        self.currentCity = newCity
+
+        # Add this road to already used
+        self.roads.append(road)
     
     def showAntDetails(self):
         print(f'Pheromone: {self.Qpheromone}')
@@ -86,6 +114,12 @@ class Ant(object):
 
     def getTotalDistance(self): 
         return self.totalDistance
+    
+    def getCurrentCity(self):
+        return self.currentCity
+    
+    def setCurrentCity(self, new_city_id):
+        self.currentCity = new_city_id
 
 class Colony(object):
     # alpha - pheromone attraction constant
@@ -101,9 +135,12 @@ class Colony(object):
         self.beta = beta
         self.p = p
         self.Q = Q
+        self.cities = 0
         self.ants = list()
         self.all_roads = list()
+
         self.initRoadsFromFile()
+        self.initAnts()
         
     def addRoad(self, road):
         self.all_roads.append(road)
@@ -111,9 +148,18 @@ class Colony(object):
     def addAnt(self, ant):
         self.ants.append(ant)
     
+    # Reads all the data from file "data.txt" and creates Road objects
     def initRoadsFromFile(self):
         f = open("data.txt", "r")
         f1 = f.readlines()
+
+        # Read amount of cities
+        cities_string = ''
+        for character in f1[0]:
+            if character.isdigit():
+                cities_string = cities_string + character
+            else:
+                self.cities = int(cities_string)
 
         for row in range(1, len(f1)):
             line = f1[row]
@@ -130,15 +176,146 @@ class Colony(object):
                         number = ''
 
             for i in range(len(distances)):
-                r = Road(distances[i], row-1, i)
-                self.addRoad(r)
+                # Don't read roads to the city itself
+                if not row-1 == i:
+                    r = Road(distances[i], row-1, i)
+                    self.addRoad(r)
+        
+        self.removeRoadDuplicates(0)
 
-    def showRoads(self):
+    def removeRoadDuplicates(self, begin_index):
+        for i in range(begin_index,len(self.all_roads)):
+            if i+1 < len(self.all_roads):
+                for j in range(i+1, len(self.all_roads)):
+                    if self.roadsEqual(self.all_roads[i], self.all_roads[j]):
+                        del self.all_roads[j]
+                        self.removeRoadDuplicates(i)
+                        return
+    
+    # Check if those 2 roads are the same
+    def roadsEqual(self, road1, road2):
+        if road1.city1_id == road2.city1_id and road1.city2_id == road2.city2_id:
+            return True
+        elif road1.city2_id == road2.city1_id and road1.city1_id == road2.city2_id:
+            return True
+        else:
+            return False
+
+    # Init ants by creating objects and giving them a random start location
+    def initAnts(self):
+        for i in range(self.size):
+            start_city_id = random.randrange(self.cities)
+            #print(start_city_id)
+            a = Ant(start_city_id, self.Q)
+            self.ants.append(a)
+
+    # update pheromone value on every road
+    def updatePheromoneValue(self):
         for road in self.all_roads:
-            print(f'Road: {road.getDistance()}')
+            deltaPheromone = 0
+            for ant in self.ants:
+                if ant.wentToRoad(road):
+                    deltaPheromone = deltaPheromone + ant.getPheromone()
+            
+            new_pheromone = self.p * road.getPheromone() + deltaPheromone
+            road.setPheromone(new_pheromone)
+    
+    def shortestRoad(self, roads):
+        shortest_road = roads[0]
+        for road in roads:
+            if road.getDistance() < shortest_road.getDistance():
+                shortest_road = road
+        return shortest_road
 
+    # Count probability for ant k to move from city i to city j
+    # Return the road with the highest probability
+    def countProbability(self, ant):
+        available_roads = list()
+        # Get all the available roads from the current city
+        for road in self.all_roads:
+            if road.getDistance() > 0:
+                if road.city1_id == ant.currentCity or road.city2_id == ant.currentCity:
+                    if ant.wentToRoad(road) == False:
+                        available_roads.append(road)
+        
+        # If there are no available roads
+        # Get back to the first city
+        if not available_roads:
+            for road in all_roads:
+                if road.city1_id == ant.start_city_id or road.city2_id == ant.start_city_id:
+                    return road
+
+        # Count denominator for the equation
+        denominator = 0
+        #print(len(available_roads))
+        for road in available_roads:
+            pheromone = road.getPheromone()
+            visibility = 1/road.getDistance()
+            product = pheromone ** self.alpha * visibility ** self.beta
+            denominator = denominator + product
+
+        # When the road has no pheromone
+        # Choose the shortest road
+        if denominator == 0:
+            return self.shortestRoad(available_roads)
+
+        # Count all probabilities
+        probabilities = list()
+        for road in available_roads:
+            pheromone = road.getPheromone()
+            visibility = 1/road.getDistance()
+            nominator = pheromone ** self.alpha * visibility ** self.beta
+            probability = nominator/denominator
+            #print(f'Probability: {probability}')
+            probabilities.append(probability)
+
+        # Get highest probability
+        highest_probability = 0
+        index_of_best_road = None
+        for i in range(len(probabilities)):
+            if probabilities[i] > highest_probability:
+                highest_probability = probabilities[i]
+                index_of_best_road = i
+        
+        best_road = available_roads[index_of_best_road]
+        return best_road 
+    
+    # Reset ants memory 
+    def resetTravelData(self):
+        for ant in self.ants:
+            ant.resetTravelData()
+
+    # Run ant colony algorithm step by step
+    def runAlgorithm(self):
+        for t in range(self.iterations):
+            for ant in self.ants:
+                for move_count in range(self.cities):
+                    new_road = self.countProbability(ant)
+                    ant.move(new_road)
+                self.updatePheromoneValue()
+                ant.showAntDetails()
+            # Reset ant memory        
+            self.resetTravelData()
+        
+        #self.showGeneralPheromoneLevel()
+    
+    def showGeneralPheromoneLevel(self):
+        for road in self.all_roads:
+            print(f'From {road.city1_id} to {road.city2_id}:')
+            print(f'Distance: {road.getDistance()}')
+            print(f'Pheromone: {road.getPheromone()}')
+            print("---")
+
+    def showFinalResult(self):
+        pass
+            
 if __name__ == "__main__":
-    road = Road(10, 1, 2)
-    a = Ant(1, 5)
-    c = Colony(1, 2, 3, 4, 5, 6)
-    c.showRoads()
+    size = 10 # Size of the colony
+    iterations = 150 # Expected iterations
+    alpha = 10 # Pheromone attraction
+    beta = 2 # Short distance attraction
+    p = 0.7 # Evaporation constant
+    Q = 5 # Ants pheromone constant
+
+    c = Colony(size, iterations, alpha, beta, p, Q)
+    c.runAlgorithm()
